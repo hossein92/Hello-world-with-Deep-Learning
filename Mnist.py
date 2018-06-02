71fd0e5d5701d4e20517c560037ec832daadd772
Hossein yousefi <y.hossein92@gmail.com>
2018-06-02 16:45:11 +0430

    Update Mnist.py

--- Mnist.py
+++ Mnist.py
@@ -11,14 +11,12 @@ from keras.layers import Dense
 from keras.utils import to_categorical
 
 
-# In[2]:
 
 
 #load Data
 (train_images, train_labels), (test_images, test_labels)=mnist.load_data()
 
 
-# In[4]:
 
 
 #PreProcessing
@@ -34,7 +32,6 @@ test_labels = to_categorical(test_labels)
 train_labels = to_categorical(train_labels)
 
 
-# In[7]:
 
 
 #Network Architector
@@ -43,7 +40,6 @@ network.add(Dense(512, activation='relu', input_shape=(784,)))
 network.add(Dense(10, activation='softmax'))
 
 
-# In[10]:
 
 
 #optemizer setting
@@ -52,23 +48,24 @@ network.compile(optimizer='rmsprop',
                metrics=['accuracy'])
 
 
-# In[12]:
+
 
 
 #Training
 network.fit(train_images,train_labels ,epochs=5,batch_size=128)
 
 
-# In[13]:
+
 
 
 # evaluation
 network.evaluate(test_images,test_labels)
 
 
-# In[15]:
 
 
+# Show Result
 (loss , accuracy ) = network.evaluate(test_images, test_labels)
-print (accuracy)
-
+print ('--------------------------------------')
+print ('your eavaluate accuracy is : ',accuracy)
+print ('--------------------------------------')
